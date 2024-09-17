94a55b4bc68353830443e3b7c4e999ded51f5d8c
Oleksandr Cherevko <oc.oleksandr.cherevko@outlook.com>
2024-09-17 17:51:02 +0300

    init commit

---
+++ Hillel_homework_1/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+	cout << "##################################################\n";
+	cout << "##\t\t\tForm\t\t\t##\n";
+	cout << "##################################################\n";
+	cout << "# Name: Cherevko Oleksandr\t\t\t #\n";
+	cout << "# Age: 27yo\t\t\t\t\t #\n";
+	cout << "# Education: Master's degree in computer science #\n";
+	cout << "# Residence: Ukraine, Dnipro\t\t\t #\n";
+	cout << "# email: oc.oleksandr.cherevko@outlook.com\t #\n";
+	cout << "# phone: +380950004496\t\t\t\t #\n";
+	cout << "##################################################\n";
+
+	return 0;
+}
